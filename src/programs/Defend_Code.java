46b78148a7e5e254f1d8a5645ad445dfbe463fdf
mattqpn <116048461+mattqpn@users.noreply.github.com>
2022-11-20 17:40:00 -0800

    added twoInts

--- src/programs/Defend_Code.java
+++ src/programs/Defend_Code.java
@@ -1,12 +1,16 @@
 package programs;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Defend_Code {
+
     static Scanner scan = new Scanner(System.in);
     public static void main(String[] args) {
+        name();
+        twoInts();
         inputFile();
     }
 
@@ -58,4 +62,46 @@ public class Defend_Code {
         }
     }
 
+    public static void twoInts() {
+        System.out.println("Enter two numbers between -2,147 to 2,147");
+        int int1 = 0, int2 = 0;
+        boolean ok = false;
+
+        // TODO needs to clean of commas
+        while (!ok) {
+            System.out.print("Enter the first number: ");
+            try {
+                if (scan.hasNextInt()) {
+                    int1 = scan.nextInt();
+                    if (int1 >= -2147 && int1 <= 2147) {
+                        ok = true;
+                    }
+                } else {
+                    scan.nextLine();
+                    System.out.println("Not a number between -2,147 to 2,147");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Try again");
+            }
+        }
+        ok = false;
+        while (!ok) {
+            System.out.print("Enter the second number: ");
+            try {
+                if (scan.hasNextInt()) {
+                    int2 = scan.nextInt();
+                    if (int2 >= -2147 && int2 <= 2147) {
+                        ok = true;
+                    }
+                } else {
+                    scan.nextLine();
+                    System.out.println("Not a number between -2,147 to 2,147");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Try again");
+            }
+        }
+
+        System.out.println(int1 + " " + int2);
+    }
 }
